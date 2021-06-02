55dfaa4404c1736382b8392456e787cdd32e0bb4
Jun Fujima <jf.devvv@gmail.com>
2021-06-03 02:36:24 +0900

    add route

--- src/pages/Ex1.js
+++ src/pages/Ex1.js
@@ -1,5 +1,38 @@
+import { useEffect, useRef } from 'react';
+
+const Bokeh = window.Bokeh;
+
 function Ex1() {
-  return <div>Ex1</div>;
+  const bokehRoot = useRef(null);
+
+  const N = 500;
+  const x = [0, 10, 20, 30];
+  const y = [0, 10, 20, 30];
+  const d = [
+    [0, 10, 20, 30],
+    [0, 10, 20, 30],
+    [0, 10, 20, 30],
+    [0, 10, 20, 30],
+  ];
+
+  useEffect(() => {
+    // create bokeh figure
+    const p = Bokeh.Plotting.figure({
+      tooltips: [
+        ['x', '$x'],
+        ['y', '$y'],
+        ['value', '@image'],
+      ],
+    });
+
+    return null;
+  }, []);
+
+  return (
+    <div>
+      <div ref={bokehRoot}></div>
+    </div>
+  );
 }
 
 export default Ex1;
